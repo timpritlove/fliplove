51f385a4821aac4538f58630fb52bdda4be0e0f9
Tim Pritlove <tim@pritlove.org>
2023-02-28 14:32:45 +0100

    Store state in struct

--- lib/flipdot/display_state.ex
+++ lib/flipdot/display_state.ex
@@ -2,26 +2,27 @@
 # - change state to struct
 
 defmodule Flipdot.DisplayState do
+  defstruct bitmap: nil
   use Agent
 
   @topic "display_update"
 
-  def start_link(initial_value) do
-    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+  def start_link(bitmap) do
+    Agent.start_link(fn -> %__MODULE__{bitmap: bitmap} end, name: __MODULE__)
   end
 
   def topic, do: @topic
 
   def get do
-    Agent.get(__MODULE__, & &1)
+    Agent.get(__MODULE__, fn state -> state.bitmap end)
   end
 
   def set(bitmap) do
-    old_bitmap = Agent.get(__MODULE__, & &1)
+    old_bitmap = get()
 
     if bitmap != old_bitmap do
       Phoenix.PubSub.broadcast(Flipdot.PubSub, @topic, {:display_update, bitmap})
-      Agent.update(__MODULE__, fn _ -> bitmap end)
+      Agent.update(__MODULE__, fn _ -> %__MODULE__{bitmap: bitmap} end)
     end
   end
 end
