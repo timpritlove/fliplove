45733869a84a862f8c2a5632b882c97c68c066e8
Tim Pritlove <tim@pritlove.org>
2023-03-03 16:32:48 +0100

    more kerning

--- lib/flipdot/kerning.ex
+++ lib/flipdot/kerning.ex
@@ -2,7 +2,10 @@ defmodule Flipdot.Kerning do
   @kerning %{
     "-Adobe-Helvetica-Bold-O-Normal--17-120-100-100-P-92-ISO8859-1" => %{
       'lu' => -1,
+      'le' => -1,
+      'nu' => -1,
       'ep' => -1,
+      'es' => -1,
       'do' => -1,
       'ol' => -1,
       'ox' => -1,
@@ -11,10 +14,18 @@ defmodule Flipdot.Kerning do
       'br' => -1,
       'th' => -1,
       'qu' => -1,
+      'tg' => -1,
+      'st' => -1,
+      'lt' => -1,
       'ui' => -1,
       'wn' => -1,
       'ov' => -1,
       'ps' => -1,
+      'zi' => -1,
+      'er' => -1,
+      'Li' => -1,
+      'li' => -1,
+      'in' => -1,
       'ot' => -1
     }
   }
