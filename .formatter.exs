14f9a2a1078a15e7a2de8cf4e413c2378484d776
Tim Pritlove <tim@pritlove.org>
2023-03-04 10:45:42 +0100

    still tryin to get formatter to not include parens (does not work yet)

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 [
   import_deps: [:phoenix],
+  locals_without_parens: [defbitmap: 1],
   plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"],
-  export: [locals_without_parens: [defbitmap: :*]]
+  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
 ]
