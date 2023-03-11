13c769dd6a0714a0f779fd7b7d99f3104268018d
Tim Pritlove <tim@pritlove.org>
2023-03-11 20:45:21 +0100

    changed line_length

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,7 @@
 [
   import_deps: [:phoenix],
   locals_without_parens: [defbitmap: 1],
+  line_length: 120,
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
 ]
