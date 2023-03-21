0951c406bb6b420d5e61afc25b652d3087d2626f
Tim Pritlove <tim@pritlove.org>
2023-03-21 02:23:22 +0100

    Initialize Scene with DisplayState

--- lib/flipdot/scene_display.ex
+++ lib/flipdot/scene_display.ex
@@ -2,15 +2,37 @@ defmodule Flipdot.SceneDisplay do
   use Scenic.Scene
   import Scenic.Primitives
   import Scenic.Components
+  alias Flipdot.DisplayState
   alias Scenic.Graph
 
   def init(scene, _param, _opts) do
-    graph =
-      Graph.build()
-      |> rect({8, 8}, stroke: {1, :yellow}, t: {100, 100})
+    graph = build_scene_graph()
 
     scene = push_graph(scene, graph)
 
     {:ok, scene}
+  end
+
+  @display_origin_x 65
+  @display_origin_y 20
+  @pixel_size 10
+  @border_size 2
+
+  defp build_scene_graph do
+    bitmap = DisplayState.get()
+
+    graph = Graph.build()
+
+    Enum.reduce(bitmap.matrix, graph, fn {{x, y}, value}, graph ->
+      rect(graph, {@pixel_size, @pixel_size},
+        stroke: {@border_size, :black},
+        fill: %{1 => :yellow, 0 => :black}[value],
+        t: {@display_origin_x + x * @pixel_size, @display_origin_y + (bitmap.height - 1 - y) * @pixel_size}
+      )
+    end)
+    |> rect({bitmap.width * @pixel_size, bitmap.height * @pixel_size},
+      stroke: {1, :yellow},
+      t: {@display_origin_x - div(@pixel_size, 2), @display_origin_y - div(@pixel_size, 2)}
+    )
   end
 end
