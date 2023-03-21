16b68431d46be84f9814cd6c898e8e27c151f1be
Tim Pritlove <tim@pritlove.org>
2023-03-21 02:47:07 +0100

    auto update scene

--- lib/flipdot/scene_display.ex
+++ lib/flipdot/scene_display.ex
@@ -1,5 +1,6 @@
 defmodule Flipdot.SceneDisplay do
   use Scenic.Scene
+  require Logger
   import Scenic.Primitives
   import Scenic.Components
   alias Flipdot.DisplayState
@@ -9,6 +10,7 @@ defmodule Flipdot.SceneDisplay do
     graph = build_scene_graph()
 
     scene = push_graph(scene, graph)
+    Phoenix.PubSub.subscribe(Flipdot.PubSub, Flipdot.DisplayState.topic())
 
     {:ok, scene}
   end
@@ -17,6 +19,12 @@ defmodule Flipdot.SceneDisplay do
   @display_origin_y 20
   @pixel_size 10
   @border_size 2
+
+  def handle_info({:display_update, _bitmap}, scene) do
+    graph = build_scene_graph()
+    scene = push_graph(scene, graph)
+    {:noreply, scene}
+  end
 
   defp build_scene_graph do
     bitmap = DisplayState.get()
@@ -30,7 +38,7 @@ defmodule Flipdot.SceneDisplay do
         t: {@display_origin_x + x * @pixel_size, @display_origin_y + (bitmap.height - 1 - y) * @pixel_size}
       )
     end)
-    |> rect({bitmap.width * @pixel_size, bitmap.height * @pixel_size},
+    |> rect({(bitmap.width + 1) * @pixel_size, (bitmap.height + 1) * @pixel_size},
       stroke: {1, :yellow},
       t: {@display_origin_x - div(@pixel_size, 2), @display_origin_y - div(@pixel_size, 2)}
     )
