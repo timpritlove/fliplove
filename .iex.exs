119a2e8999d935f9ce4fc5de8dcc0809fd33ce9c
Tim Pritlove <tim@pritlove.org>
2023-03-22 11:57:34 +0100

    load an invader

--- .iex.exs
+++ .iex.exs
@@ -1,1 +1,12 @@
 si = Bitmap.from_file("data/frames/fb_space_invaders.txt")
+invader =     Bitmap.from_lines_of_text([
+      "  X     X  ",
+      "   X   X   ",
+      "  XXXXXXX  ",
+      " XX XXX XX ",
+      "XXXXXXXXXXX",
+      "X XXXXXXX X",
+      "X X     X X",
+      "   XX XX   "
+    ])
+
