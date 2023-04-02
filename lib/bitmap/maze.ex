75080cbcf8bd814a4456112e5642ae9b0ccd3042
Tim Pritlove <tim@pritlove.org>
2023-04-03 01:12:43 +0200

    maze_solver as a stream!!!

--- lib/bitmap/maze.ex
+++ lib/bitmap/maze.ex
@@ -3,6 +3,11 @@ defmodule Bitmap.Maze do
   Generate a maze. Both width and height must be odd numbers
   """
   require Integer
+  require Logger
+
+  def generate_maze(bitmap) do
+    generate_maze(bitmap.width, bitmap.height)
+  end
 
   def generate_maze(width, height) do
     if Integer.is_even(width) or Integer.is_even(height),
@@ -57,5 +62,49 @@ defmodule Bitmap.Maze do
         |> Bitmap.set_pixel({next_x, next_y}, 0)
         |> do_generate_maze([{next_x, next_y} | visited_cells])
     end
+  end
+
+  # maze solver
+
+  def solve_maze(bitmap) do
+    Stream.resource(
+      fn -> {:dead_end, bitmap} end,
+      fn acc ->
+        case acc do
+          {:close_door, bitmap, {nx, ny}} ->
+            bitmap = bitmap |> Bitmap.set_pixel({nx, ny}, 1)
+            {[bitmap], {:dead_end, bitmap}}
+
+          {:dead_end, bitmap} ->
+            {width, height} = Bitmap.dimensions(bitmap)
+
+            # find dead ends
+
+            dead_ends =
+              for x <- Range.new(1, width - 2, 2),
+                  y <- Range.new(1, height - 2, 2),
+                  Bitmap.get_pixel(bitmap, {x, y}) == 0,
+                  open_paths =
+                    (for {nx, ny} <- [{x, y + 1}, {x, y - 1}, {x - 1, y}, {x + 1, y}],
+                         Bitmap.get_pixel(bitmap, {nx, ny}) == 0 do
+                       {nx, ny}
+                     end),
+                  length(open_paths) == 1 do
+                {{x, y}, open_paths}
+              end
+
+            case dead_ends do
+              [] ->
+                {:halt, bitmap}
+
+              dead_ends ->
+                {{x, y}, [{nx, ny} | _]} = Enum.random(dead_ends)
+                bitmap = bitmap |> Bitmap.set_pixel({x, y}, 1)
+                {[bitmap], {:close_door, bitmap, {nx, ny}}}
+            end
+        end
+      end,
+      fn foo -> foo end
+    )
   end
 end
