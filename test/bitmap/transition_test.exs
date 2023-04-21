5643dab78af1de6cc33adaa03ffb207abfe0ba1e
Tim Pritlove <tim@pritlove.org>
2023-04-21 08:40:55 +0200

    testing transitions

---
+++ test/bitmap/transition_test.exs
@@ -0,0 +1,36 @@
+defmodule BitmapTransitionTest do
+  use ExUnit.Case
+  doctest Bitmap
+
+  def invader do
+    Bitmap.from_lines_of_text([
+      "  X     X  ",
+      "   X   X   ",
+      "  XXXXXXX  ",
+      " XX XXX XX ",
+      "XXXXXXXXXXX",
+      "X XXXXXXX X",
+      "X X     X X",
+      "   XX XX   "
+    ])
+  end
+
+  def inverted_invader do
+    invader() |> Bitmap.invert()
+  end
+
+  test "push_up transition" do
+    assert Bitmap.Transition.push_up(invader(), inverted_invader()) |> Enum.at(4) |> inspect() == """
+           +-----------+
+           |XXXXXXXXXXX|
+           |X XXXXXXX X|
+           |X X     X X|
+           |   XX XX   |
+           |XX XXXXX XX|
+           |XXX XXX XXX|
+           |XX       XX|
+           |X  X   X  X|
+           +-----------+
+           """
+  end
+end
