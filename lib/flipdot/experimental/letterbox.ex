44f8f155c8ee5cd73524adbc423961f7f7b1d495
Tim Pritlove <tim@pritlove.org>
2024-01-30 16:05:22 +0100

    Initial code for TLA animation transitions

---
+++ lib/flipdot/experimental/letterbox.ex
@@ -0,0 +1,30 @@
+defmodule Flipdot.Experimental.Letterbox do
+  @transition_ms 100
+  @still_ms 1_000
+
+  alias Flipdot.Font.Renderer
+  alias Bitmap.Transition
+
+  def render_tla_movie(text) do
+    font = Flipdot.Font.Fonts.Letterbox.get()
+    letters = String.to_charlist(text)
+    render_transition(letters, font)
+  end
+
+  def render_transition([letter], font) do
+    frame = render_letter(letter, font)
+    [{@still_ms, frame}]
+  end
+
+  def render_transition([from_char | remaining_letters], font) do
+    from = render_letter(from_char, font)
+    to_char = hd(remaining_letters)
+    to = render_letter(to_char, font)
+    movie = Transition.push_up(from, to, @transition_ms) |> Enum.to_list()
+    movie ++ render_transition(remaining_letters, font)
+  end
+
+  def render_letter(char, font) do
+    Renderer.render_text(Bitmap.new(8, 8), {0, 0}, font, [char])
+  end
+end
