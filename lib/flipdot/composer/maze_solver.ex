e2a9f9ebfd0aabb9cca966a33a99a139027ed523
Tim Pritlove <tim@pritlove.org>
2024-12-14 22:39:18 +0100

    fix maze solver behaviour

--- lib/flipdot/composer/maze_solver.ex
+++ lib/flipdot/composer/maze_solver.ex
@@ -8,6 +8,9 @@ defmodule Flipdot.Composer.MazeSolver do
 
   @registry Flipdot.Composer.Registry
 
+  @frame_delay 500
+  @maze_delay 5_000
+
   defstruct maze_stream: nil
 
   def start_link(_opts) do
@@ -18,16 +21,21 @@ defmodule Flipdot.Composer.MazeSolver do
 
   @impl true
   def init(state) do
+    Logger.info("Starting Maze Solver...")
     Registry.register(@registry, :running_composer, :maze_solver)
 
-    maze = Bitmap.Maze.generate_maze(Display.width(), Display.height() - 1)
-    Display.set(maze)
+    maze_stream = new_maze_stream()
 
-    maze_stream = Bitmap.Maze.solve_maze(maze, mode: :parallel)
-
-    Process.send_after(self(), :next_frame, 1_000)
-    Logger.info("Maze Solver has been started.")
+    Process.send_after(self(), :next_frame, @maze_delay)
     {:ok, %{state | maze_stream: maze_stream}}
+  end
+
+  defp new_maze_stream do
+    width = make_odd(Display.width())
+    height = make_odd(Display.height())
+    maze = Bitmap.Maze.generate_maze(width, height)
+    Display.set(maze)
+    Bitmap.Maze.solve_maze(maze, mode: :parallel)
   end
 
   @impl true
@@ -41,16 +49,24 @@ defmodule Flipdot.Composer.MazeSolver do
       case Enum.take(state.maze_stream, 1) do
         [bitmap] ->
           Display.set(bitmap)
-          Process.send_after(self(), :next_frame, 100)
+          Process.send_after(self(), :next_frame, @frame_delay)
           Stream.drop(state.maze_stream, 1)
 
         [] ->
-          maze = Bitmap.Maze.generate_maze(Display.width(), Display.height())
-          Display.set(maze)
-          Bitmap.Maze.solve_maze(maze, mode: :parallel)
-          Process.send_after(self(), :next_frame, 5_000)
+          Process.send_after(self(), :new_maze_stream, @maze_delay)
+          []
       end
 
     {:noreply, %{state | maze_stream: maze_stream}}
   end
+
+  def handle_info(:new_maze_stream, state) do
+    new_maze_stream = new_maze_stream()
+    Process.send_after(self(), :next_frame, @maze_delay)
+    {:noreply, %{state | maze_stream: new_maze_stream }}
+  end
+
+  # Helper function to ensure dimensions are odd
+  defp make_odd(n) when rem(n, 2) == 0, do: n - 1
+  defp make_odd(n), do: n
 end
