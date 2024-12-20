21d05d9c765236471c9ac0ea35912a2fb3123ce7
Tim Pritlove <tim@pritlove.org>
2024-12-20 05:40:11 +0100

    reformat

--- .iex.exs
+++ .iex.exs
@@ -1,5 +1,5 @@
 si = Bitmap.from_file("data/frames/fb_space_invaders.txt")
-invader =     Bitmap.from_lines_of_text([
+invader = Bitmap.from_lines_of_text([
       "  X     X  ",
       "   X   X   ",
       "  XXXXXXX  ",
@@ -9,4 +9,3 @@ invader =     Bitmap.from_lines_of_text([
       "X X     X X",
       "   XX XX   "
     ])
-
