935f813ceda9617d490e2abe5ee1d27660805573
Tim Pritlove <tim@pritlove.org>
2025-11-05 10:01:48 +0100

    fix dev config for tailwind and esbuild by mentioning project name explicitly instead of default

--- config/dev.exs
+++ config/dev.exs
@@ -15,8 +15,8 @@ config :fliplove, FliploveWeb.Endpoint,
   debug_errors: true,
   secret_key_base: "yFQXM+1R6jdFpH3ip5khTBUxnP9L0JDJeRUn70wzeLpE/k8Aw8IlHfMZ4UgjdDAz",
   watchers: [
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
-    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
+    esbuild: {Esbuild, :install_and_run, [:fliplove, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:fliplove, ~w(--watch)]}
   ]
 
 # ## SSL Support
